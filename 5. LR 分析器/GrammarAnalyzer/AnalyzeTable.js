20bb9d9713d8a9ac709fa04e116515d655a68b39
Soulike <316413310@qq.com>
2018-11-04 11:55:10 +0800

    完成

--- 5. LR 分析器/GrammarAnalyzer/AnalyzeTable.js
+++ 5. LR 分析器/GrammarAnalyzer/AnalyzeTable.js
@@ -7,14 +7,13 @@ class Action
      * grammar: 这次归约使用的文法
      * errorProcessor: 错误处理函数
      * */
-    constructor(type, nextStateNumber = -1, reduceTokenNumber = 0, reduceTo = '', grammar = '', errorType = -1)
+    constructor(type, nextStateNumber = -1, reduceTokenNumber = 0, reduceTo = '', grammar = '')
     {
         this.type = type;
         this.nextStateNumber = nextStateNumber;
         this.reduceTo = reduceTo;
         this.reduceTokenNumber = reduceTokenNumber;
         this.grammar = grammar;
-        this.errorType = errorType;
     }
 
     isReduce()
@@ -46,202 +45,115 @@ class Action
     {
         return this.grammar;
     }
-
-    getErrorType()
-    {
-        return this.errorType;
-    }
 }
 
 Action.SHIFT = 'SHIFT';
 Action.REDUCE = 'REDUCE';
-Action.ERROR = 'ERROR';
-Action.ERROR_TYPE = {
-    EXTRA_INPUT: 0,
-    UNEXPECTED_END: 1,
-    UNMATCHED_RIGHT_BRACKET: 2,
-    MISSING_OPERAND: 2
-};
 
-const {SHIFT, REDUCE, ERROR} = Action;
-const {EXTRA_INPUT, UNEXPECTED_END, UNMATCHED_RIGHT_BRACKET, MISSING_OPERAND} = Action.ERROR_TYPE;
-
+const {SHIFT, REDUCE} = Action;
 const ActionTable = {
     0: {
-        '+': new Action(ERROR, -1, 0, '', '', EXTRA_INPUT),
-        '*': new Action(ERROR, -1, 0, '', '', EXTRA_INPUT),
+
         '(': new Action(SHIFT, 4),
-        ')': new Action(ERROR, -1, 0, '', '', EXTRA_INPUT),
-        'id': new Action(SHIFT, 5),
-        '#': new Action(ERROR, -1, 0, '', '', UNEXPECTED_END)
+
+        'id': new Action(SHIFT, 5)
+
     },
     1: {
         '+': new Action(SHIFT, 6),
-        '*': new Action(ERROR, -1, 0, '', '', EXTRA_INPUT),
-        '(': new Action(ERROR, -1, 0, '', '', EXTRA_INPUT),
-        ')': new Action(ERROR, -1, 0, '', '', EXTRA_INPUT),
-        'id': new Action(ERROR, -1, 0, '', '', EXTRA_INPUT),
         '#': new Action(SHIFT, -1)
     },
     2: {
         '+': new Action(REDUCE, -1, 1, 'E', 'E->T'),
         '*': new Action(SHIFT, 7),
-        '(': new Action(ERROR, -1, 0, '', '', EXTRA_INPUT),
-        ')': new Action(ERROR, -1, 0, '', '', EXTRA_INPUT),
-        'id': new Action(ERROR, -1, 0, '', '', EXTRA_INPUT),
         '#': new Action(REDUCE, -1, 1, 'E', 'E->T')
     },
     3: {
         '+': new Action(REDUCE, -1, 1, 'T', 'T->F'),
         '*': new Action(REDUCE, -1, 1, 'T', 'T->F'),
-        '(': new Action(ERROR, -1, 0, '', '', EXTRA_INPUT),
-        ')': new Action(ERROR, -1, 0, '', '', EXTRA_INPUT),
-        'id': new Action(ERROR, -1, 0, '', '', EXTRA_INPUT),
         '#': new Action(REDUCE, -1, 1, 'T', 'T->F')
     },
     4: {
-        '+': new Action(ERROR, -1, 0, '', '', EXTRA_INPUT),
-        '*': new Action(ERROR, -1, 0, '', '', EXTRA_INPUT),
         '(': new Action(SHIFT, 11),
-        ')': new Action(ERROR, -1, 0, '', '', EXTRA_INPUT),
-        'id': new Action(SHIFT, 12),
-        '#': new Action(ERROR, -1, 0, '', '', UNEXPECTED_END)
+        'id': new Action(SHIFT, 12)
     },
     5: {
         '+': new Action(REDUCE, -1, 1, 'F', 'F->id'),
         '*': new Action(REDUCE, -1, 1, 'F', 'F->id'),
-        '(': new Action(ERROR, -1, 0, '', '', EXTRA_INPUT),
-        ')': new Action(ERROR, -1, 0, '', '', EXTRA_INPUT),
-        'id': new Action(ERROR, -1, 0, '', '', EXTRA_INPUT),
         '#': new Action(REDUCE, -1, 1, 'F', 'F->id')
     },
     6: {
-        '+': new Action(ERROR, -1, 0, '', '', EXTRA_INPUT),
-        '*': new Action(ERROR, -1, 0, '', '', EXTRA_INPUT),
         '(': new Action(SHIFT, 4),
-        ')': new Action(ERROR, -1, 0, '', '', EXTRA_INPUT),
-        'id': new Action(SHIFT, 5),
-        '#': new Action(ERROR, -1, 0, '', '', UNEXPECTED_END)
+        'id': new Action(SHIFT, 5)
     },
     7: {
-        '+': new Action(ERROR, -1, 0, '', '', EXTRA_INPUT),
-        '*': new Action(ERROR, -1, 0, '', '', EXTRA_INPUT),
         '(': new Action(SHIFT, 4),
-        ')': new Action(ERROR, -1, 0, '', '', EXTRA_INPUT),
-        'id': new Action(SHIFT, 5),
-        '#': new Action(ERROR, -1, 0, '', '', UNEXPECTED_END)
+        'id': new Action(SHIFT, 5)
     },
     8: {
         '+': new Action(SHIFT, 16),
-        '*': new Action(ERROR, -1, 0, '', '', EXTRA_INPUT),
-        '(': new Action(ERROR, -1, 0, '', '', EXTRA_INPUT),
-        ')': new Action(SHIFT, 15),
-        'id': new Action(ERROR, -1, 0, '', '', EXTRA_INPUT),
-        '#': new Action(ERROR, -1, 0, '', '', UNEXPECTED_END)
+        ')': new Action(SHIFT, 15)
     },
     9: {
         '+': new Action(REDUCE, -1, 1, 'E', 'E->T'),
         '*': new Action(SHIFT, 17),
-        '(': new Action(ERROR, -1, 0, '', '', EXTRA_INPUT),
-        ')': new Action(REDUCE, -1, 1, 'E', 'E->T'),
-        'id': new Action(ERROR, -1, 0, '', '', EXTRA_INPUT),
-        '#': new Action(ERROR, -1, 0, '', '', UNEXPECTED_END)
+        ')': new Action(REDUCE, -1, 1, 'E', 'E->T')
     },
     10: {
         '+': new Action(REDUCE, -1, 1, 'T', 'T->F'),
         '*': new Action(REDUCE, -1, 1, 'T', 'T->F'),
-        '(': new Action(ERROR, -1, 0, '', '', EXTRA_INPUT),
-        ')': new Action(REDUCE, -1, 1, 'T', 'T->F'),
-        'id': new Action(ERROR, -1, 0, '', '', EXTRA_INPUT),
-        '#': new Action(ERROR, -1, 0, '', '', UNEXPECTED_END)
+        ')': new Action(REDUCE, -1, 1, 'T', 'T->F')
     },
     11: {
-        '+': new Action(ERROR, -1, 0, '', '', EXTRA_INPUT),
-        '*': new Action(ERROR, -1, 0, '', '', EXTRA_INPUT),
         '(': new Action(SHIFT, 11),
-        ')': new Action(ERROR, -1, 0, '', '', UNMATCHED_RIGHT_BRACKET),
-        'id': new Action(SHIFT, 12),
-        '#': new Action(ERROR, -1, 0, '', '', UNEXPECTED_END)
+        'id': new Action(SHIFT, 12)
     },
     12: {
         '+': new Action(REDUCE, -1, 1, 'F', 'F->id'),
         '*': new Action(REDUCE, -1, 1, 'F', 'F->id'),
-        '(': new Action(ERROR, -1, 0, '', '', EXTRA_INPUT),
-        ')': new Action(REDUCE, -1, 1, 'F', 'F->id'),
-        'id': new Action(ERROR, -1, 0, '', '', EXTRA_INPUT),
-        '#': new Action(ERROR, -1, 0, '', '', UNEXPECTED_END)
+        ')': new Action(REDUCE, -1, 1, 'F', 'F->id')
     },
     13: {
         '+': new Action(REDUCE, -1, 3, 'E', 'E->E+T'),
         '*': new Action(SHIFT, 7),
-        '(': new Action(ERROR, -1, 0, '', '', EXTRA_INPUT),
-        ')': new Action(ERROR, -1, 0, '', '', EXTRA_INPUT),
-        'id': new Action(ERROR, -1, 0, '', '', EXTRA_INPUT),
         '#': new Action(REDUCE, -1, 3, 'E', 'E->E+T')
     },
     14: {
         '+': new Action(REDUCE, -1, 3, 'T', 'T->T*F'),
         '*': new Action(REDUCE, -1, 3, 'T', 'T->T*F'),
-        '(': new Action(ERROR, -1, 0, '', '', EXTRA_INPUT),
-        ')': new Action(ERROR, -1, 0, '', '', EXTRA_INPUT),
-        'id': new Action(ERROR, -1, 0, '', '', EXTRA_INPUT),
         '#': new Action(REDUCE, -1, 3, 'T', 'T->T*F')
     },
     15: {
         '+': new Action(REDUCE, -1, 3, 'F', 'F->(E)'),
         '*': new Action(REDUCE, -1, 3, 'F', 'F->(E)'),
-        '(': new Action(ERROR, -1, 0, '', '', EXTRA_INPUT),
-        ')': new Action(ERROR, -1, 0, '', '', EXTRA_INPUT),
-        'id': new Action(ERROR, -1, 0, '', '', EXTRA_INPUT),
         '#': new Action(REDUCE, -1, 3, 'F', 'F->(E)')
     },
     16: {
-        '+': new Action(ERROR, -1, 0, '', '', EXTRA_INPUT),
-        '*': new Action(ERROR, -1, 0, '', '', EXTRA_INPUT),
         '(': new Action(SHIFT, 11),
-        ')': new Action(ERROR, -1, 0, '', '', EXTRA_INPUT),
-        'id': new Action(SHIFT, 12),
-        '#': new Action(ERROR, -1, 0, '', '', UNEXPECTED_END)
+        'id': new Action(SHIFT, 12)
     },
     17: {
-        '+': new Action(ERROR, -1, 0, '', '', EXTRA_INPUT),
-        '*': new Action(ERROR, -1, 0, '', '', EXTRA_INPUT),
         '(': new Action(SHIFT, 11),
-        ')': new Action(ERROR, -1, 0, '', '', EXTRA_INPUT),
-        'id': new Action(SHIFT, 12),
-        '#': new Action(ERROR, -1, 0, '', '', UNEXPECTED_END)
+        'id': new Action(SHIFT, 12)
     },
     18: {
         '+': new Action(SHIFT, 16),
-        '*': new Action(ERROR, -1, 0, '', '', EXTRA_INPUT),
-        '(': new Action(ERROR, -1, 0, '', '', EXTRA_INPUT),
-        ')': new Action(SHIFT, 21),
-        'id': new Action(ERROR, -1, 0, '', '', EXTRA_INPUT),
-        '#': new Action(ERROR, -1, 0, '', '', UNEXPECTED_END)
+        ')': new Action(SHIFT, 21)
     },
     19: {
         '+': new Action(REDUCE, -1, 3, 'E', 'E->E+T'),
         '*': new Action(SHIFT, 17),
-        '(': new Action(ERROR, -1, 0, '', '', EXTRA_INPUT),
-        ')': new Action(REDUCE, -1, 3, 'E', 'E->E+T'),
-        'id': new Action(ERROR, -1, 0, '', '', EXTRA_INPUT),
-        '#': new Action(ERROR, -1, 0, '', '', UNEXPECTED_END)
+        ')': new Action(REDUCE, -1, 3, 'E', 'E->E+T')
     },
     20: {
         '+': new Action(REDUCE, -1, 3, 'T', 'T->T*F'),
         '*': new Action(REDUCE, -1, 3, 'T', 'T->T*F'),
-        '(': new Action(ERROR, -1, 0, '', '', EXTRA_INPUT),
-        ')': new Action(REDUCE, -1, 3, 'T', 'T->T*F'),
-        'id': new Action(ERROR, -1, 0, '', '', EXTRA_INPUT),
-        '#': new Action(ERROR, -1, 0, '', '', UNEXPECTED_END)
+        ')': new Action(REDUCE, -1, 3, 'T', 'T->T*F')
     },
     21: {
         '+': new Action(REDUCE, -1, 3, 'F', 'F->(E)'),
         '*': new Action(REDUCE, -1, 3, 'F', 'F->(E)'),
-        '(': new Action(ERROR, -1, 0, '', '', EXTRA_INPUT),
-        ')': new Action(REDUCE, -1, 3, 'F', 'F->(E)'),
-        'id': new Action(ERROR, -1, 0, '', '', EXTRA_INPUT),
-        '#': new Action(ERROR, -1, 0, '', '', UNEXPECTED_END)
+        ')': new Action(REDUCE, -1, 3, 'F', 'F->(E)')
     }
 };
 
@@ -282,13 +194,24 @@ function getAction(stateNumber, terminal)
     return ActionTable[stateNumber][terminal];
 }
 
+function getActionsForStateNumber(stateNumber)
+{
+    return ActionTable[stateNumber];
+}
+
 function getGoto(stateNumber, nonTerminal)
 {
     return GotoTable[stateNumber][nonTerminal];
 }
 
+function getGotoForStateNumber(stateNumber)
+{
+    return GotoTable[stateNumber];
+}
+
 module.exports = {
     getAction,
     getGoto,
-    ERROR_TYPE: Action.ERROR_TYPE
+    getActionsForStateNumber,
+    getGotoForStateNumber
 };
--- 5. LR 分析器/GrammarAnalyzer/GrammarAnalyzer.js
+++ 5. LR 分析器/GrammarAnalyzer/GrammarAnalyzer.js
@@ -1,6 +1,6 @@
 const {LexicalAnalyzer} = require('../LexicalAnalyzer');
 const {Element} = require('./Element');
-const {getAction, getGoto} = require('./AnalyzeTable');
+const {getAction, getGoto, getActionsForStateNumber, getGotoForStateNumber} = require('./AnalyzeTable');
 
 class GrammarAnalyzer
 {
@@ -19,9 +19,18 @@ class GrammarAnalyzer
         let token = this.getNextToken();
         while (true)
         {
+            if (token === undefined)
+            {
+                return;
+            }
             const tokenValue = token.getValue();
             const action = getAction(this.top().getStateNumber(), tokenValue);
-            if (action.isShift())
+            if (action === undefined)
+            {
+                this.errorProcessor();
+                token = this.getNextToken();
+            }
+            else if (action.isShift())
             {
                 if (action.getNextStateNumber() === -1)
                 {
@@ -82,13 +91,59 @@ class GrammarAnalyzer
         return this.analyzeProcess;
     }
 
-    extraInputError(extraTerminal)
+    errorProcessor()
     {
-        return () =>
+        const token = this.tokenArray[this.currentTokenIndex];
+        this.analyzeProcessAppend(`错误：在 ${token.getCol()} 列处 ${token.getValue()} 出现错误`);
+        this.currentTokenIndex--;
+        while (true) // 退栈循环
         {
-            this.analyzeProcessAppend(`错误: 多余的输入符号 ${extraTerminal}`);
-            this.getNextToken();
-        };
+            const topStateNumber = this.top().getStateNumber();// 查看栈顶状态
+            const gotoForTopStateNumber = getGotoForStateNumber(topStateNumber);
+            let hasGotValidToken = false;
+            if (gotoForTopStateNumber !== undefined) // 如果栈顶状态有非终结符的转移
+            {
+                while (true) // 忽略输入符号循环
+                {
+                    const currentTokenValue = this.getNextToken().getValue();
+                    if (currentTokenValue === '#') // 忽略到了结束符，那就结束错误恢复尝试
+                    {
+                        this.analyzeProcessAppend(`错误：意外的代码结尾`);
+                        return;
+                    }
+                    for (const nonTerminal in gotoForTopStateNumber)
+                    {
+                        if (gotoForTopStateNumber.hasOwnProperty(nonTerminal))
+                        {
+                            const stateNumber = gotoForTopStateNumber[nonTerminal];
+                            if (getAction(stateNumber, currentTokenValue) !== undefined) // 如果发现转移之后可以恢复正常分析
+                            {
+                                this.currentTokenIndex--;
+                                this.push(new Element(stateNumber, nonTerminal));
+                                hasGotValidToken = true; // 告知已经可以恢复正常分析
+                                break;
+                            }
+                        }
+                    }
+                    if (hasGotValidToken) // 如果找到了可以继续分析的终结符，就跳出循环
+                    {
+                        break;
+                    }
+                    else // 否则，忽略这个输入
+                    {
+                        this.analyzeProcessAppend(`错误：跳过输入符号 ${currentTokenValue}`);
+                    }
+                }
+                if (hasGotValidToken)
+                {
+                    break;
+                }
+            }
+            else // 如果没有，就继续出栈
+            {
+                this.pop();
+            }
+        }
     }
 }
 
